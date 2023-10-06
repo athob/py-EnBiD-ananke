6b3bb87e805b6ae4eb91627f15f08ad51f59ef20
Adrien Thob <adrien.thob@gmail.com>
2023-10-06 18:54:16 -0400

    Next dev version

--- src/EnBiD_ananke/__metadata__.py
+++ src/EnBiD_ananke/__metadata__.py
@@ -6,9 +6,9 @@ __all__ = ['__author__', '__copyright__', '__credits__', '__license__', '__versi
 
 __author__ = "Adrien Thob"
 __copyright__ = None
-__credits__ = ["Adrien Thob"]
+__credits__ = ["Adrien Thob", "Robyn Sanderson", "Andrew Eden"]
 __license__ = "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)"
-__version__ = "0.1.2b2"
+__version__ = "0.1.3.dev1"
 __maintainer__ = "Adrien Thob"
 __email__ = "athob@sas.upenn.edu"
-__status__ = "Development Status :: 1 - Planning"
+__status__ = "Development Status :: 4 - Beta"
