9932d08fe4c4ac828941c36bc9b3e5fbacf6c21e
Adrien Thob <adrien.thob@gmail.com>
2024-08-28 01:27:40 -0400

    Better configuration for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,8 @@ release = md.__version__
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinx_math_dollar',
+    'sphinx.ext.mathjax',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
@@ -36,6 +38,7 @@ extensions = [
 
 exclude_patterns = ['.ipynb_checkpoints/*']
 
+napoleon_use_param = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
