deed2afd72b8025d57b1db42e796ab1696af40b6
Adrien Thob <adrien.thob@gmail.com>
2025-09-08 22:48:20 -0400

    Last change to the header notices

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 # Author: Adrien CR Thob
 # Copyright (C) 2022  Adrien CR Thob
 #
-# This file is part of py-EnBiD-ananke:
-# <https://github.com/athob/py-EnBiD-ananke>.
+# This file is part of the py-EnBiD-ananke project,
+# <https://github.com/athob/py-EnBiD-ananke>,  which is licensed
+# under the GNU General Public License v2.0 (GPL-2.0).
 # 
 # The full copyright notice, including terms governing use, modification,
 # and redistribution, is contained in the files LICENSE and COPYRIGHT,
